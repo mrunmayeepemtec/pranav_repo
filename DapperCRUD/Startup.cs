3805878cea0cf65fadb410d2d48e2d0d76708524
Pranav Dani <Pranav.Dani@emtecinc.com>
2022-11-28 22:39:42 +0530

    added Authorization And Authentication in rest Api

--- DapperCRUD/Startup.cs
+++ DapperCRUD/Startup.cs
@@ -1,83 +1,83 @@
-//using DapperCRUD.Models;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Text;
+using DapperCRUD.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-//namespace DapperCRUD
-//{
-//    public class Startup
-//    {
-//        public Startup(IConfiguration configuration)
-//        {
-//            Configuration = configuration;
-//        }
+namespace DapperCRUD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-//        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-//        // This method gets called by the runtime. Use this method to add services to the container.
-//        public void ConfigureServices(IServiceCollection services)
-//        {
-//            services.AddControllers();
-//            services.AddDbContext<UserContex>
-//     (options => options.UseSqlServer(Configuration.GetConnectionString("StudentConn")));
-//            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-//            {
-//                options.RequireHttpsMetadata = false;
-//                options.SaveToken = true;
-//                options.TokenValidationParameters = new TokenValidationParameters()
-//                {
-//                    ValidateIssuer = true,
-//                    ValidateAudience = true,
-//                    ValidAudience = Configuration["Jwt:Audience"],
-//                    ValidIssuer = Configuration["Jwt:Issuer"],
-//                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-//                };
-//            });
-//            //services.AddDbContext<UserContex>
-//            //     (options => options.UseSqlServer(Configuration.GetConnectionString("StudentConn")));
-//            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-//            // {
-//            //     options.RequireHttpsMetadata = false;
-//            //     options.SaveToken = true;
-//            //     options.TokenValidationParameters = new TokenValidationParameters()
-//            //     {
-//            //         ValidateIssuer = true,
-//            //         ValidateAudience = true,
-//            //         ValidAudience = Configuration["Jwt:Audience"],
-//            //         ValidIssuer = Configuration["Jwt:Issuer"],
-//            //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-//            //     };
-//            // });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<UserContex>
+     (options => options.UseSqlServer(Configuration.GetConnectionString("StudentConn")));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+            //services.AddDbContext<UserContex>
+            //     (options => options.UseSqlServer(Configuration.GetConnectionString("StudentConn")));
+            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            // {
+            //     options.RequireHttpsMetadata = false;
+            //     options.SaveToken = true;
+            //     options.TokenValidationParameters = new TokenValidationParameters()
+            //     {
+            //         ValidateIssuer = true,
+            //         ValidateAudience = true,
+            //         ValidAudience = Configuration["Jwt:Audience"],
+            //         ValidIssuer = Configuration["Jwt:Issuer"],
+            //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+            //     };
+            // });
 
-//        }
-
-
+        }
 
 
-//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//        {
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//            }
 
-//            app.UseHttpsRedirection();
 
-//            app.UseRouting();
-//            app.UseAuthentication();    
-//            app.UseAuthorization();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-//            app.UseEndpoints(endpoints =>
-//            {
-//                endpoints.MapControllers();
-//            });
-//        }
-//    }
-//}
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthentication();    
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
